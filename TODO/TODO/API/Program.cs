d1feb2a14053be443285f0d07c064106f08665e9
GUSTAVO FERNANDES TOLEDO <gustavo.toledo@cs.up.edu.br>
2025-06-05 09:37:29 -0300

    GET id

--- TODO/TODO/API/Program.cs
+++ TODO/TODO/API/Program.cs
@@ -7,14 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDataContext>();
 var app = builder.Build();
 
-GET/api/tarefasommi
+GET/api/tarefas
 app.MapGet("api/tarefas", ([FromServices] AppDataContext ctx) => 
 {
     var tarefas = ctx.tarefas.include (t => t.status).toList();
     results.Ok(tarefa): results.NotFound();
 });
 
-
+GET/api/tarefas/{id}
+app.MapGet("api/tarefas/{id}",([FromRoute]string id, [FromServices] AppDataContext ctx)=>
+{
+    var tarefa = ctx.tarefa.find(id);
+    return tarefa == null ?
+    results.NotFound();
+    results.Ok(tarefa);
+});
 
 
 app.Run();
